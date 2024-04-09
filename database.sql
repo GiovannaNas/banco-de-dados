eef3cc75a5dac07ec611729ed4bb4ebd0dc80a30
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-04-08 21:12:09 -0300

    Initial commit

---
+++ database.sql
@@ -0,0 +1,23 @@
+select * from gafanhotos g 
+join gafanhoto_assiste_curso a
+on g.id = a.idgafanhoto; 
+
+select  g.nome, c.nome from gafanhotos g 
+join gafanhoto_assiste_curso a
+on g.id = a.idgafanhoto
+join cursos c
+on c.idcurso = a.idcurso
+order by g.nome; 
+
+
+
+ 
+
+
+
+
+
+ 
+
+
+
